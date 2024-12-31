c2e7dc36edfa2036e13e806d671673fc8f6d60c1
anthonymtru <126616109+anthonymtru@users.noreply.github.com>
2024-12-31 06:14:12 -0600

    Add files via upload

--- tft_stats_checker.py
+++ tft_stats_checker.py
@@ -1,18 +1,24 @@
 import pandas as pd
 
+# Reads spreadsheet
 df = pd.read_excel('tft_data.xlsx')
+
+# General game counter
 games_count = df['Placement'].count()
 
+# Calculates the Win Rate statistic
 def winrate_stat():
     wins_count = df['Win'].sum()
     percent = (wins_count * 100.0)/games_count
     return 'Your win rate is ' + str(percent) + '%.'
 
+# Calculates the Top 4 Rate Statistic
 def top4_rate_stat():
     top4_count = df['Top 4'].sum()
     percent = (top4_count * 100.0)/games_count
     return 'Your top 4 rate is ' + str(percent) + '%.'
 
+# Prompt that allows user to get into augment stats
 def augment_stats():
     user_input = input('What augment would you like to check? ').lower()
     # Filter the DataFrame where the augment appears in any of the three augment columns
@@ -21,8 +27,10 @@ def augment_stats():
         (df['Augment 2'].str.lower() == user_input) |
         (df['Augment 3'].str.lower() == user_input)
     ]
+    # General count for amount of games in filtered data
     filtered_games = filtered_df['Win'].count()
     
+    # Gathers augment of choice and runs stats based on desired input
     user_input = input('What would you like to check? (Top4Rate/Winrate/AvgPlacement) ').lower()
     if user_input == 'top4rate':
         top4_count = filtered_df['Top 4'].sum()
